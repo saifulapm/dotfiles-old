7e5107581382087b4a196dd38f4eef88e4d74eea
Saiful Islam <saiful.apm@gmail.com>
2021-04-06 09:09:31 +0600

    Php lsp added

--- bin/install.sh
+++ bin/install.sh
@@ -266,6 +266,7 @@ ok
 
 bot "Install neovim"
 npm i -g bash-language-server
+npm i -g intelephense
 require_brew  luajit --HEAD
 require_brew neovim --HEAD
 running "Configruation nvim"
@@ -280,6 +281,16 @@ running "Install vim plugins"
 cd ~/.config/nvim
 make install
 cd -
+
+bot "Composer Install"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+mv composer.phar /usr/local/bin/composer
+composer global require php-stubs/wordpress-globals
+composer global require php-stubs/wordpress-stubs
+composer global require php-stubs/woocommerce-stubs
 
 action "Install Shopify Staff"
 brew tap shopify/shopify
